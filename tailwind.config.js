259650eec1bab1a39930b7a0379187e5cfc53f36
Galeriku Digitalshop <mazfahuwaiza@gmail.com>
2022-04-09 16:34:30 +0700

    Zpa Project Upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '15px',
+    },
+    extend: {
+      colors: {
+        primary: '#5b21b6',
+        secondary: '#64748b',
+        dark: '#171717',
+      },
+      screen: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
